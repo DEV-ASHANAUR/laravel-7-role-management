22dc1f465a1d7329b890376e9ce3497d78f1fedd
dev-ashanaur <ashanour009@gmail.com>
2021-03-05 00:30:23 +0600

    admin templeate mastaring done

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::prefix('admin')->group(function () {
+    Route::get('/', 'Backend\DashboardController@index')->name('admin.dashboard');
+});
